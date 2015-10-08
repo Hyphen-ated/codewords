f634711a8de70e1b3ff49631163e94fce49667b4
mike <mdrob@mdrob.com>
2015-10-08 11:55:34 -0500

    Initial implementation
    Will draw cards and words on a canvas.

---
+++ main.js
@@ -0,0 +1,33 @@
+/* vim: set ts=4 sw=4 tw=4 et : */
+
+const cwidth = 111;
+const mwidth = cwidth / 2;
+const cheight = 155;
+const mheight = cheight / 2;
+const spacing = 10;
+const rounded = 10;
+
+function to2d(n) {
+    var row = n % 5;
+    var col = Math.floor(n / 5);
+    return {
+        x : row * (spacing + cwidth) + spacing,
+        y : col * (spacing + cheight) + spacing
+    }
+}
+
+function drawwords(context) {
+    var words = getParameter('words').split(',', 25);
+    for (var i = 0; i < words.length; i++) {
+        var p = to2d(i);
+        roundRect(context, p.x, p.y, cwidth, cheight, rounded);
+        context.fillText(words[i], p.x + mwidth, p.y + mheight); 
+    }
+}
+
+$(function(){
+    var canvas = document.getElementById('c');
+    var context = canvas.getContext('2d');
+    context.textAlign = 'center';
+    drawwords(context);
+});
